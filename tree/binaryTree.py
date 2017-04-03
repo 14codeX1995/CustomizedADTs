f0f8a500e1ed8ffd7394a8cb8a1ec221bea5c701
14codeX1995 <serrao.ryan1995@gmail.com>
2017-04-03 10:12:48 +0200

    B tree updation and file(level order) deletion

--- tree/binaryTree.py
+++ tree/binaryTree.py
@@ -1,7 +1,14 @@
+from queue import queueADT
 
-class BinaryTree:
+class BinaryTreeNode:
     def __init__(self,data):
         self.data=data
         self.left=None
         self.right=None
-        
+
+class BinaryTree:
+    def __init__(self,data=None):
+        self.root=BinaryTreeNode(data)
+
+    def insertUsingLevelOrder(root, data):
+
--- util/levelOrderInsertion.py
+++
@@ -1,7 +0,0 @@
-
-from tree.binaryTree import BinaryTree
-
-
-def insertUsingLevelOrder(root,data,treeType):
-    if treeType=="binaryTree":
-        newNode=BinaryTree(data)
